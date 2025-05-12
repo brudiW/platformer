edf6083f49e9ab197e93aae4d87c3d52c44b0bb8
brudiW <146438483+brudiW@users.noreply.github.com>
2025-05-12 14:17:33 +0200

    item.py erstellen

---
+++ scripts/item.py
@@ -0,0 +1,33 @@
+import pygame
+
+class Item:
+    def __init__(self, name, description, effect, texture, rarety):
+        self.name = name
+        self.description = description
+        self.effect = effect
+        self.texture = texture
+        self.rarety = rarety
+    
+class ShopItem(Item):
+    def __init__(self, name, description, effect, texture, rarety, price):
+        
+        self.price = price
+        super(name, description, effect, texture, rarety)
+        
+        
+        
+class CollectableItem(Item):
+    def __init__(self, name, description, effect, texture, rarety):
+        
+        super(name, description, effect, texture, rarety)
+    
+    
+class OwnedItem(Item):
+    def __init__(self, name, description, effect, texture, rarety, durability):
+        
+        self.durability = durability
+        super(name, description, effect, texture, rarety)
+        
+    def use(self):
+        if not self.durability == -1 and self.durability > 0:
+            self.durability -= 1
