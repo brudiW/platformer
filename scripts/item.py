ea1c522fa43813eb375eed22c9e49224c1ba2935
Undertale010607 <damian.dezius.new.01@gmail.com>
2025-05-16 10:37:59 +0200

    Update item.py

--- scripts/item.py
+++ scripts/item.py
@@ -24,46 +24,76 @@ class Items:
         f.close()
         self.itemlist = map_data['items']
         
-        for item in self.itemlist.values():
+        for item in self.itemlist:
             if item["type"] == "shop":
-                self.add_item(ShopItem(item['name'], item['description'], item['effects'], item, item['rarety'], item['price']))
+                self.add_item(ShopItem(item['name'], item['description'], item['effects'], "assets/images/items/" + item['texture'] + ".png", item['rarety'], item['price'], self.game))
             elif item['type'] == 'collectable':
-                self.add_item(CollectableItem(item['name'], item['description'], item['effects'], item, item['rarety']))
+                self.add_item(CollectableItem(item['name'], item['description'], item['effects'], "assets/images/items/" + item['texture'] + ".png", item['rarety'], self.game))
             elif item['type'] == 'owned':
-                self.add_item(OwnedItem(item['name'], item['description'], item['effects'], item, item['rarety'], item['durability']))
+                self.add_item(OwnedItem(item['name'], item['description'], item['effects'], "assets/images/items/" + item['texture'] + ".png", item['rarety'], item['durability'], self.game))
+    def list_items(self, sub):
+        print("Items:")
+        if sub == 'shop':
+            for item in self.shop_items.values():
+                print(f" - {item.name}: {item.description} (Price: {item.price})")
+                self.showItem(item.texture)
+        elif sub == 'collectable':
+            for item in self.collectables.values():
+                print(f" - {item.name}: {item.description}")
+        elif sub == 'owned':
+            for item in self.owned_items.values():
+                print(f" - {item.name}: {item.description} (Durability: {item.durability})")
+        else:
+            print("Invalid category. Available categories: shop, collectable, owned")
+            return
+        print("All items:")
+        for item in self.items.values():
+            print(f" - {item.name}: {item.description}")
+
+    def showItem(self, texture):
+        return pygame.image.load(texture), (100, 100)
 class Item:
-    def __init__(self, name, description, effect, texture, rarety):
+    def __init__(self, name, description, effect, texture, rarety, game):
         self.name = name
         self.description = description
         self.effect = effect
         self.texture = texture
         self.rarety = rarety
+        self.game = game
+    
 
     def load_texture(self):
-        if self.texture in self.game.assets:
-            return self.game.assets[self.texture]
-        else:
-            raise ValueError(f"Texture {self.texture} not found in assets")
+        return self.texture
     
 class ShopItem(Item):
-    def __init__(self, name, description, effect, texture, rarety, price):
-        super().__init__(name, description, effect, texture, rarety)
+    def __init__(self, name, description, effect, texture, rarety, price, game):
+        super().__init__(name, description, effect, texture, rarety, game)
         self.price = price
         
         
         
         
 class CollectableItem(Item):
-    def __init__(self, name, description, effect, texture, rarety):
+    def __init__(self, name, description, effect, texture, rarety, game):
         
-        super().__init__(name, description, effect, texture, rarety)
+        super().__init__(name, description, effect, texture, rarety, game)
     
     
 class OwnedItem(Item):
-    def __init__(self, name, description, effect, texture, rarety, durability):
-        super().__init__(name, description, effect, texture, rarety)
+    def __init__(self, name, description, effect, texture, rarety, durability, game):
+        super().__init__(name, description, effect, texture, rarety, game)
         self.durability = durability
         
     def use(self):
         if not self.durability == -1 and self.durability > 0:
             self.durability -= 1
+    
+    def equip(self, slot):
+        if self.durability == 0:
+            print("Item is broken")
+            return False
+        else:
+            print("Item equipped")
+            self.game.item_slots[slot] = self
+            return True
+            
