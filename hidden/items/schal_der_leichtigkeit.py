dbcb7de36e1adc8816bdd0fc50c772fa01488925
brudiW <146438483+brudiW@users.noreply.github.com>
2025-05-19 12:15:25 +0200

    Update schal_der_leichtigkeit.py

--- hidden/items/schal_der_leichtigkeit.py
+++ hidden/items/schal_der_leichtigkeit.py
@@ -14,6 +14,11 @@ def hook():
     if player.collisions['down']:
         jump_state['jump_count'] = 0
 
+    if game.btnA > 0.5: # JUMP
+        if jump_state['jump_count'] < jump_state['max_jumps']:
+            player.velocity[1] = -2.7
+            jump_state['jump_count'] += 1
+
     if keys[pygame.K_SPACE]:
         if not jump_state['jump_pressed']:
             if jump_state['jump_count'] < jump_state['max_jumps']:
@@ -22,5 +27,6 @@ def hook():
         jump_state['jump_pressed'] = True
     else:
         jump_state['jump_pressed'] = False
+    
 
 register_hook(hook)
